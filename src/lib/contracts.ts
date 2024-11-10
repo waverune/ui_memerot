5eba3b71ca6eb65a68d051b71648c93666d99e4a
TheThinker277 <f20191482@hyderabad.bits-pilani.ac.in>
2024-11-10 14:26:41 +0530

    added-address-to-multichain

--- src/lib/contracts.ts
+++ src/lib/contracts.ts
@@ -20,7 +20,7 @@ export const MULTICALL_ADDRESSES: Record<number, string> = {
   1: '0xcA11bde05977b3631167028862bE2a173976CA11', // Ethereum Mainnet
   56: '0xcA11bde05977b3631167028862bE2a173976CA11', // BSC
   137: '0xcA11bde05977b3631167028862bE2a173976CA11', // Polygon
-  21598:'0x8413176534f9bd6c85f143c30c8b66ae50904457' 
+  21598:'0xbe5442b465A6BF98ba30b449087Ee821eb1eE3cC' 
   // Add other networks as needed
 };
 // Mock Swap Contract Interface
