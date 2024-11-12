4530a9998489a7b94a38741a370c44cfcb1d89a2
waverune <yashviknaithani@gmail.com>
2024-11-12 19:11:17 +0530

    update quoter

--- src/lib/quoter_utils.ts
+++ src/lib/quoter_utils.ts
@@ -50,7 +50,7 @@ const publicClient = createPublicClient({
     chain: defineChain(BuildBearChain),
     transport: http()
 })
-const quoterAddress = '0x1664A211D6C2414c88671a412065A15388EFEd5d' // Replace with your actual quoter address
+const quoterAddress = '0xeB79c476Abb5623bCc3F1b8A61EE3FE6a2700B7D' // Replace with your actual quoter address
 
 // Quoters returns array of token amounts in their respective decimals
 // for human readable amounts, divide by 10^decimals eg decimals: 18 for ETH, divide by 10^18, usdc: 6, divide by 10^6, spx&hpos: 8, divide by 10^8
